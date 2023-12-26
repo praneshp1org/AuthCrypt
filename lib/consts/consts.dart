c208845a042bafd7c8b28bbac78ff7277967b0f5
Pranesh Pyara Shrestha <info.praneshpyara@gmail.com>
2023-12-26 23:32:16 +0545

    necessary styles and constants as in the UI prototype

---
+++ lib/consts/consts.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: constant_identifier_names
+
+class Consts {
+  static const THEME_STATUS = "THEME_STATUS";
+  static const APP_NAME = "AuthCrypt";
+  // static const LOGO = 'assets/logo.png';
+  static const TITLE_1 = 'Welcome to AuthCrypt!';
+  static const TITLE_2 = 'Secured Authentication';
+  static const SUBTITLE_1 = 'AuthCrypt is the most secure way to '
+      'store and protect your passwords '
+      'from cybercriminals.';
+  static const SUBTITLE_2 =
+      'AuthCrypt uses a Advanced Encryption Standard(AES) '
+      'to protect your passwords. '
+      'Your passwords are private even from us.';
+}
